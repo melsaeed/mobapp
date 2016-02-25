71d7c102684c92af887948205d36e160f0000728
Muhammad El-Saeed <m.elsaeed@brightery.com>
2016-02-25 18:15:31 +0200

    Update

--- app/app.js
+++ app/app.js
@@ -9,38 +9,38 @@ var safa = angular.module('safa', [
 ]);
 safa.config(function ($routeProvider, $httpProvider) {
     $routeProvider.
-        //when('/welcome_screen', {templateUrl: 'templates/welcome_screen.html', controller: 'welcome_screenCtrl'}).
+        when('/welcome_screen', {templateUrl: 'templates/welcome_screen.html', controller: 'welcome_screenCtrl'}).
         when('/login', {templateUrl: 'templates/login.html', controller: 'loginCtrl'}).
-        //when('/logout', {templateUrl: 'templates/login.html',controller: 'logoutCtrl'}).
-        //when('/forget_pass', {templateUrl: 'templates/forget_pass.html', controller: 'forgetPasswordCtrl'}).
-        //when('/registration', {templateUrl: 'templates/registration.html', controller: 'registrationCtrl'}).
-        //when('/activity_dashboard', {templateUrl: 'templates/activity_dashboard.html', controller: 'activity_dashboardCtrl'}).
-        //when('/main_dashboard', {templateUrl: 'templates/main_dashboard.html', controller: 'main_dashboardCtrl'}).
-        //when('/main', {templateUrl: 'templates/main.html', controller: 'mainCtrl'}).
+        when('/logout', {templateUrl: 'templates/login.html',controller: 'logoutCtrl'}).
+        when('/forget_pass', {templateUrl: 'templates/forget_pass.html', controller: 'forgetPasswordCtrl'}).
+        when('/registration', {templateUrl: 'templates/registration.html', controller: 'registrationCtrl'}).
+        when('/activity_dashboard', {templateUrl: 'templates/activity_dashboard.html', controller: 'activity_dashboardCtrl'}).
+        when('/main_dashboard', {templateUrl: 'templates/main_dashboard.html', controller: 'main_dashboardCtrl'}).
+        when('/main', {templateUrl: 'templates/main.html', controller: 'mainCtrl'}).
         when('/users', {templateUrl: 'templates/users.html', controller: 'usersCtrl'}).
-        //when('/circles', {templateUrl: 'templates/circles.html', controller: 'circlesCtrl'}).
-        //when('/edit_group/:id', {templateUrl: 'templates/edit_group.html', controller: 'edit_groupCtrl'}).
-        //when('/create_group', {templateUrl: 'templates/edit_group.html', controller: 'create_groupCtrl'}).
-        ////when('/circle_comp/:type/:id', {templateUrl: 'templates/transport_emp.html', controller: 'circle_compCtrl'}).
-        //when('/team_lastactivity', {templateUrl: 'templates/team_lastactivity.html', controller: 'team_lastactivityCtrl'}).
-        //when('/company_orders', {templateUrl: 'templates/company_orders.html', controller: 'company_ordersCtrl'}).
+        when('/circles', {templateUrl: 'templates/circles.html', controller: 'circlesCtrl'}).
+        when('/edit_group/:id', {templateUrl: 'templates/edit_group.html', controller: 'edit_groupCtrl'}).
+        when('/create_group', {templateUrl: 'templates/edit_group.html', controller: 'create_groupCtrl'}).
+        when('/circle_comp/:type/:id', {templateUrl: 'templates/transport_emp.html', controller: 'circle_compCtrl'}).
+        when('/team_lastactivity', {templateUrl: 'templates/team_lastactivity.html', controller: 'team_lastactivityCtrl'}).
+        when('/company_orders', {templateUrl: 'templates/company_orders.html', controller: 'company_ordersCtrl'}).
         otherwise({redirectTo: '/login'});
-    //$httpProvider.defaults.withCredentials = true;
-    //$httpProvider.interceptors.push('httpRequestInterceptor');
+    $httpProvider.defaults.withCredentials = true;
+    $httpProvider.interceptors.push('httpRequestInterceptor');
 });
 
-//safa.factory('httpRequestInterceptor', function ($cookies) {
-//    return {
-//        request: function (config) {
-//          console.log($cookies.get('token'));
-//            config.headers['X-Auth-Token'] = $cookies.get('token');//window.localStorage.getItem("token");
-//            return config;
-//        }
-//    };
-//});
+safa.factory('httpRequestInterceptor', function ($cookies) {
+    return {
+        request: function (config) {
+          console.log($cookies.get('token'));
+            config.headers['X-Auth-Token'] = $cookies.get('token');//window.localStorage.getItem("token");
+            return config;
+        }
+    };
+});
 
-//safa.run(['$http', function ($http) {
-//    $http.defaults.withCredentials = true;
+safa.run(['$http', function ($http) {
+    $http.defaults.withCredentials = true;
 //    //if(typeof token != 'undefined' && token != '')
 //    //if ($cookies.get('token'))
 //    //    $http.defaults.headers.common['X-Auth-Token'] = $cookies.get('token');
@@ -48,4 +48,4 @@ safa.config(function ($routeProvider, $httpProvider) {
 //    //    $http.defaults.headers.common['X-Auth-Token'] = window._token;
 //
 //    //$http.defaults.headers.post['X-CSRFToken'] = $cookies.csrftoken;
-//}]);
+}]);
