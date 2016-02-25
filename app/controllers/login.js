39691105f011ec2d0825cd5519fbc5656232dd74
Muhammad El-Saeed <m.elsaeed@brightery.com>
2016-02-25 19:16:27 +0200

    Update

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -2,14 +2,14 @@ safa.controller('loginCtrl', function ($scope, $http, $location, $cookies) {
 
     $scope.credintials = {email: "", password: ""};
     $scope.login = function () {
-        loading.start();
+        //loading.start();
         $http({
             url: api_url + 'auth/login',
             withCredentials: true,
             params: {email: $scope.credintials.email, password: calcMD5($scope.credintials.password)},
             method: 'GET'
         }).then(function (response) {
-            loading.stop();
+            //loading.stop();
             if (response.data.error) {
                 error.fire(response.data.message);
                 response.data.token
