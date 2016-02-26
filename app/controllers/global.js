d870bdd7f3a9a8e6db3fdd3f7e8231d41598056c
Muhammad El-Saeed <m.elsaeed@brightery.com>
2016-02-26 22:01:23 +0200

    Update

--- app/controllers/global.js
+++ app/controllers/global.js
@@ -1,6 +1,22 @@
 
-safa.controller('forgetPasswordCtrl', function () {
-
+safa.controller('forgetPasswordCtrl', function ($scope, $http) {
+    $scope.forget_password = function () {
+        if ($scope.email.length == 0) {
+            error.fire("»—Ìœ ≈·ﬂ —Ê‰Ì €Ì— ’ÕÌÕ");
+        }
+        else {
+            $http({
+                url: api_url + 'user/forget_password',
+                method: 'post',
+                data: {email: $scope.email}
+            }).then(function (response) {
+                if (response.data.error)
+                    error.fire("»—Ìœ ≈·ﬂ —Ê‰Ì €Ì— „ÿ«»ﬁ");
+                else
+                    error.fire(" „ «—”«· »—Ìœ ≈·ﬂ —Ê‰Ì · ﬁÊ„ » €ÌÌ— ﬂ·„… «·„—Ê—° ‘ﬂ—« ·ﬂ");
+            });
+        }
+    }
 });
 
 safa.controller('activity_dashboardCtrl', function () {
